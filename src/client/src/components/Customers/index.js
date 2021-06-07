7c068f1d12f03f7c688253898561b072f5c2c423
donald_h <donald_h@USMB128986.local>
2021-06-07 07:12:23 -0700

    fixed proxy error with docker port in react package-json

--- src/client/src/components/Customers/index.js
+++ src/client/src/components/Customers/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
+
 export default class Customers extends Component {
     state = { customers: [] }
 
     componentDidMount() {
-        fetch('/api/customers')
+        fetch('/api/customers', function(req, res){console.log(req)})
             .then(res => { 
-                console.log(res);
+                JSON.parse(res);
+                console.log(res)
                 // res.json();
                 
-            })
+            }).catch(function(err){console.log(err)})
             // .then(customers => this.setState({ customers }, () => {
             //     console.log(customers)
             // }
--- src/server.ts
+++ src/server.ts
@@ -8,16 +8,19 @@ const func = require('./simple/func');
 
 //Middleware1
 //Static public folder
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.get('/', (req: Request, res: Response, next: NextFunction) => res.send("number is: " + func(2, 3)));
 app.get('/api/customers', (req: Request, res: Response, next: NextFunction) => {
-    const customers = [
+    const customers: Object = [
         { id: 1, first: 'john', last: 'doe' },
         { id: 2, first: 'jane', last: 'doe' },
         { id: 3, first: 'bob', last: 'jones' }
     ];
+    console.log(customers)
+    console.log(req.body)
+    // console.log(res)
     res.json(customers);
 });
 
