b9fa07b828e7736a2d1c127377c920c247f301eb
donald_h <donald_h@USMB128986.local>
2021-06-08 13:52:45 -0700

    added a route and fetch for simple func

--- src/client/src/components/Customers/index.js
+++ src/client/src/components/Customers/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './customers.css';
 
 export default class Customers extends Component {
-    state = { customers: [] }
+    state = { customers: [], func: null }
     componentDidMount() {
         fetch('/api/customers')
             .then(res => {
@@ -10,6 +10,17 @@ export default class Customers extends Component {
             }).catch(function (err) { console.log(err) })
             .then(data => {
                 this.setState({ customers: data })
+                console.log(data)
+            })
+
+        fetch('/api/func')
+            .then(res => {
+                console.log(res.body)
+                return res.json();
+            }).catch(function (err) { console.log(err) })
+            .then(data => {
+                console.log(data)
+                this.setState({ func: data })
                 console.log(data)
             })
     }
@@ -22,6 +33,8 @@ export default class Customers extends Component {
                     <li key={customer.id}>{customer.first} {customer.last}</li>
                 )}
             </ul>
+            <h1>Func</h1>
+            <div>{this.state.func}</div>
         </div>);
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,18 @@ import path from 'path';
 const app: Application = express();
 const func = require('./simple/func');
 
-//Middleware1
+//Middleware
 //Static public folder
 app.use(express.static(path.join(__dirname, '/client/public')));
 app.use(express.static("public"));
 
-//Routes
-app.get('/api/func', (req: Request, res: Response, next: NextFunction) => res.send("number is: " + func(2, 3)));
+//Express Routes
+app.get('/api/func', (req: Request, res: Response, next: NextFunction) => {
+    const f = "number is: " + func(2, 3);
+    console.log(f);
+    res.send(f);
+
+});
 app.get('/api/customers', (req: Request, res: Response, next: NextFunction) => {
     const customers: Object = [
         { id: 1, first: 'john', last: 'doe' },
